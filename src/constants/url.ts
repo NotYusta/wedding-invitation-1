3892f87a6a64b8fda0792ff4938a25286e645293
unknown <xabhista19@gmail.com>
2025-02-09 00:31:38 +0700

    feat: a

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,3 +1,3 @@
 export const URLConstants = {
-    BASE: "/wedding-invitation-1"
+    BASE: "/"
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-import { fileURLToPath, URL } from "url";
 // https://vite.dev/config/
 
-const urlBase = "/wedding-invitation-1"
+const urlBase = "/"
 export default defineConfig({
   base: urlBase,
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
   plugins: [react(),  tailwindcss(),],
 })
